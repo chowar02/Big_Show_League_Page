f5a849e56137fe65628dbdc22933cd13c7357b36
chowar02 <135249439+chowar02@users.noreply.github.com>
2023-08-11 19:58:20 -0400

    Update leagueInfo.js
    Added phil & pat take 2

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -202,7 +202,7 @@ export const managers = [
       "location": "Boston", // (optional)
       "bio": "Big sports fan, but we're not entirely sure he knows this league exists",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2010?, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2010, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win...eventually", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
